29de769fbf21bda5938100099982cf55ae7bf54d
LitianD <948167236@qq.com>
2019-04-15 16:23:22 +0800

    add User Entity

---
+++ src/main/java/com/bjtu/j2ee/sport_club/domain/User.java
@@ -0,0 +1,45 @@
+package com.bjtu.j2ee.sport_club.domain;
+
+
+import lombok.Data;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Entity
+@Data
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+
+	@NotEmpty(message ="username is required")
+	private String username;
+
+	@NotEmpty(message ="password is required")
+	private String password;
+
+	@NotEmpty(message ="name is required")
+	private String name;
+
+	@NotEmpty(message ="mail is required")
+	private String mail;
+
+	@Size(min = 11, max = 11, message = "Mobile no. must be 11 digits.")
+	private String phoneNumber;
+
+	private Integer age;
+
+	//0为普通用户 1为会员
+	private Integer role;
+
+	//1为男 0为女
+	private Integer sex;
+
+	@ManyToMany
+	@JoinTable(name = "user_course",joinColumns = @JoinColumn(name = "user_id"),
+			inverseJoinColumns = @JoinColumn(name = "authority_id"))
+	private List<Course> CourseList;
+}
