e64402b70d5ff48cc5497729f4e69baee98903fa
Administrator <4597889>
2019-05-10 21:33:47 +0800

    add Swagger2 to the project

--- src/main/java/com/bjtu/j2ee/sport_club/controller/CourseController.java
+++ src/main/java/com/bjtu/j2ee/sport_club/controller/CourseController.java
@@ -6,9 +6,14 @@ import com.bjtu.j2ee.sport_club.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping("/course")
+@Api(value = "CourseController|一个用于课程信息管理的控制器")
 public class CourseController {
 	CourseService courseService;
 
@@ -19,8 +24,13 @@ public class CourseController {
 	}
 
 	@Cacheable(value = "courseList")
-	@RequestMapping(value = {"/list/{page}/{size}","/courses/{page}/{size}/"})
+	@RequestMapping(value = {"/list/{page}/{size}","/courses/{page}/{size}/"},method = RequestMethod.GET)
 	@ResponseBody
+	@ApiOperation(value="获取课程列表", notes="对返回结果进行分页")
+	@ApiImplicitParams({
+			@ApiImplicitParam(paramType="path", name = "page", value = "页面数量", required = true, dataType = "Integer"),
+			@ApiImplicitParam(paramType="path", name = "size", value = "页面大小", required = true, dataType = "Integer"),
+	})
 	public ResponseJson getCourses(@PathVariable Integer page, @PathVariable Integer size)
 	{
 
@@ -28,8 +38,10 @@ public class CourseController {
 	}
 
 	@Cacheable(value = "courseInfo", key = "#id")
-	@RequestMapping(value = {"/show/{id}","/course/show/{id}/"})
+	@RequestMapping(value = {"/show/{id}","/course/show/{id}/"},method = RequestMethod.POST)
 	@ResponseBody
+	@ApiOperation(value="获取某个课程内容", notes="仅返回指定课程的内容")
+	@ApiImplicitParam(paramType="path", name = "id", value = "课程ID", required = true, dataType = "Integer")
 	public ResponseJson getCourseContent (@PathVariable Integer id)
 	{
 
@@ -37,15 +49,19 @@ public class CourseController {
 	}
 
 	@Cacheable(value = "/user_courses", key = "#reqMyCourse.username")
-	@RequestMapping(value = {"/mycourses","/mycourses/"})
+	@RequestMapping(value = {"/mycourses","/mycourses/"},method = RequestMethod.POST)
 	@ResponseBody
+	@ApiOperation(value="获取指定用户的课程列表", notes="返回用户订阅的所有的课程")
+	@ApiImplicitParam(paramType="path", name = "reqMyCourse", value = "用户请求Bean", required = true, dataType = "ReqMyCourse")
 	public ResponseJson getUserCourse(@RequestBody ReqMyCourse reqMyCourse)
 	{
 		return courseService.getMyCourse(reqMyCourse);
 	}
 
-	@RequestMapping(value = {"/user_add","/user_add/"})
+	@RequestMapping(value = {"/user_add","/user_add/"},method = RequestMethod.POST)
 	@ResponseBody
+	@ApiOperation(value="用户增加了某课程", notes="返回成功或失败")
+	@ApiImplicitParam(paramType="path", name = "reqAddUserCourse", value = "用户订阅的课程信息", required = true, dataType = "ReqAddUserCourse")
 	public ResponseJson addUserCourse(@RequestBody ReqAddUserCourse reqAddUserCourse)
 	{
 		return courseService.addUserCourse(reqAddUserCourse);
--- src/main/java/com/bjtu/j2ee/sport_club/controller/UserController.java
+++ src/main/java/com/bjtu/j2ee/sport_club/controller/UserController.java
@@ -3,14 +3,15 @@ package com.bjtu.j2ee.sport_club.controller;
 import com.bjtu.j2ee.sport_club.ResJsonBean.ResponseJson;
 import com.bjtu.j2ee.sport_club.ReqJsonBean.*;
 import com.bjtu.j2ee.sport_club.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
+@Api(value = "UserController|一个用于用户信息管理的控制器")
 public class UserController {
 	UserService userServiceTest;
 
@@ -21,24 +22,30 @@ public class UserController {
 	}
 
 
-	@RequestMapping(value = {"/signup","/signup/"})
+	@RequestMapping(value = {"/signup","/signup/"},method = RequestMethod.POST)
 	@ResponseBody
+	@ApiOperation(value="用户注册", notes="验证用户注册信息是否合规并返回结果")
+	@ApiImplicitParam(paramType="path", name = "reqSignup", value = "用户注册信息Bean", required = true, dataType = "ReqSignup")
 	public ResponseJson signUp(@RequestBody ReqSignup reqSignup)
 	{
 
 		return userServiceTest.createUser(reqSignup);
 	}
 
-	@RequestMapping(value = {"/signin","/signin/"})
+	@RequestMapping(value = {"/signin","/signin/"},method = RequestMethod.POST)
 	@ResponseBody
+	@ApiOperation(value="用户登录", notes="验证用户登录信息是够正确并返回结果")
+	@ApiImplicitParam(paramType="path", name = "reqSignin", value = "用户登录信息Bean", required = true, dataType = "ReqSignin")
 	public ResponseJson signIn(@RequestBody ReqSignin reqSignin)
 	{
 		return userServiceTest.searchUser(reqSignin);
 	}
 
 
-	@RequestMapping(value = {"/update","/update/"})
+	@RequestMapping(value = {"/update","/update/"},method = RequestMethod.POST)
 	@ResponseBody
+	@ApiOperation(value="用户个人信息更新", notes="接受并更改数据库中用户信息")
+	@ApiImplicitParam(paramType="path", name = "reqUpdate", value = "用户个人信息Bean", required = true, dataType = "ReqUpdate")
 	public ResponseJson update(@RequestBody ReqUpdate reqUpdate)
 	{
 		return userServiceTest.updateUser(reqUpdate);
